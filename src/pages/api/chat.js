f67c960fd126e87360e61cb3cfe3c82829c20c57
Gaurav Singh <83450530+Gaurav07Singh@users.noreply.github.com>
2023-07-17 03:00:43 +0530

    Update chat.js

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).json({ message: 'Method should be POST' });
   } else if (process.env.NODE_ENV !== "development") {
-    if (!referer || referer !== process.env.APP_URL) {
+    if (!referer || referer !== process.env.https://ai-bot-phi.vercel.app/) {
       res.status(401).json({ message: 'Unauthorized' });
     }
   }
